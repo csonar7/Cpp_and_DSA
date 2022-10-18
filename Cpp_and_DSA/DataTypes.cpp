eb73adc663027982c8b40d4eb242520a65b7a249
Chetan Sonar <csonar7@gmail.com>
2022-10-18 16:23:59 +0530

    first DSA

---
+++ Cpp_and_DSA/DataTypes.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+
+    int a;
+    a=21;
+
+    cout<<"size of int "<<sizeof(a)<<endl;
+
+    float b;
+    cout<<"size of float "<<sizeof(b)<<endl;
+
+    short int si;
+    long int li;
+    cout<<"size of short int "<<sizeof(si)<<endl;
+    cout<<"size of long int "<<sizeof(li)<<endl;
+    return 0;
+    }
